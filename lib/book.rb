04ab05a95bd9d00b99cced0e184812cc33327692
ralph1786 <ralph1786@gmail.com>
2018-12-12 17:35:31 -0500

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -39,3 +39,19 @@ class Book
     end
 end
 
+#----------------------------------
+#Refactor code above using attr_accessor
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
+
